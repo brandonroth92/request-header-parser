399841316fdce804a67c31a92746fcd0092e03e7
Brandon Roth <brandon.roth892@gmail.com>
2017-02-22 12:01:45 -0500

    Added comments to server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+//Use port 1337 by default
 app.set('port', (process.env.PORT || 1337));
 
+//Server index file located in 'client' directory
 app.use(express.static(path.resolve(__dirname, 'client')));
 app.get('/api/whoami', function(req, res) {
   
+  //Set object properties using request headers
   var results = {
     ipaddress: req.headers['x-forwarded-for'] || 
      req.connection.remoteAddress || 
@@ -16,8 +19,10 @@ app.get('/api/whoami', function(req, res) {
     software: req.headers['user-agent'].split(') ')[0].split(' (')[1]
   }
   
+  //If error with results objet, send internal server error
   if (!results) {
     res.sendStatus(500);
+  //Otherwise, return results
   } else {
     res.writeHeader(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(results));
